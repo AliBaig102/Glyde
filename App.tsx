a6e7488dad29289023f14b0ee7c288ec51ca8047
NowPoshAliBaig <nowposh36@gmail.com>
2025-07-31 20:42:52 +0500

    Integrate Redux for state management and add persistence support. Updated App component to include Redux Provider and PersistGate. Added auth slice and configured Redux store with AsyncStorage for persistence. Updated package dependencies for Redux and AsyncStorage.

--- App.tsx
+++ App.tsx
@@ -1,27 +1,27 @@
 /* eslint-disable react-native/no-inline-styles */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { StatusBar, useColorScheme } from 'react-native';
 import './global.css';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AlertNotificationRoot } from 'react-native-alert-notification';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './src/redux';
 import { AppNavigator } from './src/navigation';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <AlertNotificationRoot>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <AppNavigator />
-      </AlertNotificationRoot>
-    </GestureHandlerRootView>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <AlertNotificationRoot>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+            <AppNavigator />
+          </AlertNotificationRoot>
+        </GestureHandlerRootView>
+      </PersistGate>
+    </Provider>
   );
 }
 
---
+++ src/redux/index.tsx
@@ -0,0 +1,39 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import authSlice from './slices/authSlice';
+
+const rootReducer = combineReducers({
+    auth: authSlice,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: [], // Only persist these reducers
+  blacklist: [], // Don't persist these reducers
+};
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+// Create persistor
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
