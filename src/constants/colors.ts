adb7d06a548c343b871492ff8e3e6a9775deccad
NowPoshAliBaig <nowposh36@gmail.com>
2025-08-01 20:28:30 +0500

    Add color constants and update Tailwind configuration for new color palette; enhance global CSS with root variables for consistent theming

---
+++ src/constants/colors.ts
@@ -0,0 +1,33 @@
+interface Colors {
+  foodOrange: string;
+  foodDarkGrey: string;
+  foodWhite: string;
+  glydeBlue: string;
+  glydeWhite: string;
+  lightGrey: string;
+  veryLightGrey: string;
+  glydeDarkBlue: string;
+  ratingStarYellow: string;
+  glydeGrey: string;
+  glydeRed: string;
+  successGreen: string;
+  spinDriveLightBlue: string;
+  glydeLightBlue: string;
+}
+
+export const colors: Colors = {
+  foodOrange: '#FF8B06FF',
+  foodDarkGrey: '#303030FF',
+  foodWhite: '#FFFFFFFF',
+  glydeBlue: '#0465AAFF',
+  glydeWhite: '#FFFFFFFF',
+  lightGrey: '#F5F5F5FF',
+  veryLightGrey: '#FAFAFAFF',
+  glydeDarkBlue: '#061B38FF',
+  ratingStarYellow: '#F7DB32FF',
+  glydeGrey: '#EFEFEFFF',
+  glydeRed: '#FF4F4FFF',
+  successGreen: '#5CB85CFF',
+  spinDriveLightBlue: '#5BBBFFFF',
+  glydeLightBlue: '#B6E1FFFF',
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   content: ["./App.tsx", "./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "food-orange": "#FF8B06FF",
+        "food-dark-grey": "#303030FF", 
+        "food-white": "#FFFFFFFF",
+        "glyde-blue": "#0465AAFF",
+        "glyde-white": "#FFFFFFFF",
+        "light-grey": "#F5F5F5FF",
+        "very-light-grey": "#FAFAFAFF",
+        "glyde-dark-blue": "#061B38FF",
+        "ratingstar-yellow": "#F7DB32FF",
+        "glyde-grey": "#EFEFEFFF",
+        "glyde-red": "#FF4F4FFF",
+        "success-green": "#5CB85CFF",
+        "spindrive-light-blue": "#5BBBFFFF",
+        "glyde-light-blue": "#B6E1FFFF"
+      },
+    },
   },
   plugins: [],
 }
